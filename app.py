c5f78fbba1488ac0a107fda08a300edcf6b525aa
Bhavya2205 <65015099+Bhavya2205@users.noreply.github.com>
2021-05-11 15:18:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from PIL import image
+import pickle
+import numpy as np
+import matplotlob.pyplot as plt
+import pandas as pd
+st.set_option('deprecation.showfileUploaderEncoding',False)
+#load pickle model
+model=pickle.load(open('Naive_Bayes_education_model.pkl','rb'))
+datadet=pd.read_xlsx('Classification Dataset2.xlsx')
+X=datadet.iloc[:,0:-1].values
